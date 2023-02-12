7d45d1ffc4b5e0b1155c3d85d366c156706210f3
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-02-12 16:18:22 -0400

    Changed from grid to flex for better scaling

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import '../src/SCSS/styles.scss';
 
 const checkButton = document.querySelector('.check-button');
 checkButton.addEventListener('click', function() {
-  this.classList.toggle('checked');
+    this.classList.toggle('checked');
 });
+
+const addButton = document.querySelector('.addButton');
+addButton.addEventListener('click'. function() {
+
+});
+
+const createNewTodo = () => {
+
+    let newTodo = document.createElement('div');
+}
