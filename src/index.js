260eb4b10606a1724093cc68841bfed5d9440cc3
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-02-15 14:12:22 -0400

    Added new todo functionality and the ability to remove

--- src/index.js
+++ src/index.js
@@ -1,16 +1,65 @@
 import '../src/SCSS/styles.scss';
 
-const checkButton = document.querySelector('.check-button');
+//Finished task
+const checkButton = document.querySelector('.finished');
+const checkContainer = document.querySelector('.check-button');
 checkButton.addEventListener('click', function() {
-    this.classList.toggle('checked');
+    checkContainer.classList.toggle('checked');
 });
 
+//Add new task
 const addButton = document.querySelector('.addButton');
-addButton.addEventListener('click'. function() {
-
+addButton.addEventListener('click', function() {
+    createNewTodo('todo');
 });
 
-const createNewTodo = () => {
+//Remove task
+const deleteButton = document.querySelector('.trash');
+deleteButton.addEventListener('click', function() {
+    this.parentNode.parentNode.remove();
+});
 
-    let newTodo = document.createElement('div');
+const createNewTodo = (name) => {
+
+    let newTodo = document.createElement('button');
+    newTodo.classList.add('check-button');
+    
+    let leftHalf = document.createElement('div');
+    leftHalf.classList.add('leftHalf');
+
+    let img = document.createElement('img');
+    img.src = "../images/Coffee-Cup-Silhouette.svg";
+    leftHalf.appendChild(img);
+    
+    let para = document.createElement('p');
+    para.innerHTML = name;
+    leftHalf.appendChild(para);
+
+    let rightHalf = document.createElement('div');
+    rightHalf.classList.add('rightHalf');
+
+    let date = document.createElement('p');
+    date.classList.add('date');
+    date.innerHTML = 'Date goes here';
+    rightHalf.appendChild(date);
+
+    let edit = document.createElement('p');
+    edit.classList.add('edit');
+    edit.innerHTML = 'Edit goes here';
+    rightHalf.appendChild(edit);
+
+    let trash = document.createElement('p');
+    trash.classList.add('trash');
+    trash.innerHTML = 'Trash goes here';
+    trash.addEventListener('click', function() {
+        trash.parentNode.parentNode.remove();
+    });
+    rightHalf.appendChild(trash);
+
+    newTodo.appendChild(leftHalf);
+    newTodo.appendChild(rightHalf)
+
+    const toDoContainer = document.querySelector('.todo');
+    toDoContainer.appendChild(newTodo);
+    toDoContainer.appendChild(newTodo);
 }
