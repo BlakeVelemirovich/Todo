9c2e84869c400aa58f17fa6d1b3e3989d708e17f
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-03-08 11:33:11 -0400

    Added popup functionality.

--- src/index.js
+++ src/index.js
@@ -19,6 +19,64 @@ deleteButton.addEventListener('click', function() {
     this.parentNode.parentNode.remove();
 });
 
+//Edit feature
+const editPopup = (name) => {
+    
+    const popup = document.querySelector('.edit-popup');
+    popup.className = 'edit-popup-visible';
+
+    const editField = document.createElement('div');
+    editField.className = 'editField';
+
+    let editName = document.createElement('textarea');
+    editName.innerHTML = name;
+    editField.appendChild(editName);
+    
+    const dateContainer = document.createElement('div');
+    dateContainer.classList = 'dateContainer';
+    let datePara = document.createElement('p');
+    datePara.textContent = 'Select Date: ';
+    dateContainer.appendChild(datePara);
+    let selectDate = document.createElement('input');
+    selectDate.type = 'date';
+    dateContainer.appendChild(selectDate);
+    editField.appendChild(dateContainer);
+
+    const priorityContainer = document.createElement('div');
+    priorityContainer.classList = 'priorityContainer';
+    priorityContainer.appendChild(paraConstruct('Low'));
+    priorityContainer.appendChild(paraConstruct('Medium'));
+    priorityContainer.appendChild(paraConstruct('High'));
+
+    editField.appendChild(createBtn('X'));
+
+    const bottomDiv = document.createElement('div');
+    bottomDiv.classList = 'bottomDiv';
+    bottomDiv.appendChild(priorityContainer);
+    bottomDiv.appendChild(createBtn('Confirm'));
+    editField.appendChild(bottomDiv);
+
+    popup.appendChild(editField);
+
+}
+
+//Button Constructor
+const createBtn = (name) => {
+
+    let btn = document.createElement('input');
+    btn.classList = name;
+    btn.innerText = name;
+    btn.type = 'button';
+    return btn;
+}
+
+//P element constructor function
+const paraConstruct = (name) => {
+    
+    let pElement = document.createElement('p');
+    pElement.innerText = name;
+    return pElement;
+}
 
 //Todo constructor function
 const createNewTodo = (name) => {
@@ -34,6 +92,7 @@ const createNewTodo = (name) => {
     leftHalf.appendChild(img);
     
     let para = document.createElement('p');
+    para.classList.add('name');
     para.innerHTML = name;
     leftHalf.appendChild(para);
 
@@ -48,6 +107,10 @@ const createNewTodo = (name) => {
     let edit = document.createElement('p');
     edit.classList.add('edit');
     edit.innerHTML = 'Edit goes here';
+    edit.addEventListener('click', function() {
+        const nameValue = document.querySelector('.name').textContent;
+        editPopup(nameValue);
+    });
     rightHalf.appendChild(edit);
 
     let trash = document.createElement('p');
